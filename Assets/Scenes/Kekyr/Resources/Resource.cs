578afedf19e017d7c918133a97d1e28525565d1f
Mikhail Ostronosov <supermanmgdn@gmail.com>
2024-08-17 22:01:45 +0300

    Completed Heal

--- Assets/Scenes/Kekyr/Resources/Resource.cs
+++ Assets/Scenes/Kekyr/Resources/Resource.cs
@@ -14,6 +14,13 @@ public class Resource : MonoBehaviour
     private SpriteRenderer _spriteRenderer;
     private Rigidbody _rigidbody;
 
+    private Transform _movePos;
+    private float _moveForce;
+    private float _startMoveDist;
+    private float _scaleModif;
+    private bool _isInit = false;
+    private bool _isMove = false;
+
     public ResourceSO Data => _data;
 
     private void OnEnable()
@@ -28,15 +35,49 @@ public class Resource : MonoBehaviour
         _spriteRenderer.sprite = _data.Image;
     }
 
-    public IEnumerator MoveTo(Harvester harvester)
+    public void Init(Harvester harvester)
     {
-        while (enabled)
+        _movePos = harvester.transform;
+        _moveForce = harvester.MoveForce;
+        _startMoveDist = harvester.StartMoveDistance;
+        _scaleModif = harvester.ScaleModifier;
+        _isInit = true;
+    }
+
+    public void TargetReach()
+    {
+        _isMove = false;
+        Destroy(gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        if(_isMove)
         {
-            Vector3 finalTarget = new Vector3(harvester.transform.position.x, harvester.transform.position.y + _offsetY,
-                harvester.transform.position.z);
+            Vector3 finalTarget = new Vector3(_movePos.position.x, _movePos.position.y + _offsetY,
+                _movePos.position.z);
             Vector3 direction = (finalTarget - transform.position).normalized;
-            _rigidbody.AddForce(direction * harvester.MoveForce);
-            yield return null;
+            _rigidbody.linearVelocity = direction * _moveForce;
+            
+            float scale = transform.localScale.x - _scaleModif;
+            if (scale <= 0)
+            {
+                scale = 0;
+            }
+            transform.localScale = new Vector3(scale, scale, scale);
+
+            return;
+        }
+
+        if (_isInit)
+        {
+            float currentDistance = (_movePos.position - transform.position).magnitude;
+            
+            if (currentDistance <= _startMoveDist)
+            {
+                _isMove = true;
+            }
         }
     }
+
 }
