b10ac8e52c1f2049261a9375af66d08c1678ac6b
Mikhail Ostronosov <supermanmgdn@gmail.com>
2024-08-18 13:33:24 +0300

    Added healthbar

--- Assets/Code/GamerLogic/HealthView.cs
+++ Assets/Code/GamerLogic/HealthView.cs
@@ -1,22 +1,33 @@
 using System;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class HealthView : MonoBehaviour
 {
-    [SerializeField] private Health _health;
-    [SerializeField] private float _speed;
+    private readonly float FadeOutValue = 0;
+    private readonly float FadeInValue = 1;
 
-    private Coroutine _changeValue;
+    [SerializeField] private Health _health;
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private float _changeDuration;
+    [SerializeField] private float _fadeDuration;
+    [SerializeField] private float _timerDuration;
+    
     private Slider _slider;
-    private float _filled;
+    private bool _isTimerOn;
+    private float _timer;
 
     private void OnEnable()
     {
         if (_health == null)
         {
             throw new ArgumentNullException(nameof(_health));
+        }
+
+        if (_canvasGroup == null)
+        {
+            throw new ArgumentNullException(nameof(_canvasGroup));
         }
 
         _health.HealthChanged += OnHealthChanged;
@@ -26,27 +37,31 @@ public class HealthView : MonoBehaviour
     {
         _health.HealthChanged -= OnHealthChanged;
     }
+    
+    private void Update()
+    {
+        if (_isTimerOn)
+        {
+            _timer -= Time.deltaTime;
+
+            if (_timer <= 0)
+            {
+                _canvasGroup.DOFade(FadeOutValue, _fadeDuration);
+                _isTimerOn = false;
+            }
+        }
+    }
 
     private void Awake()
     {
         _slider = GetComponent<Slider>();
     }
-
+    
     public void OnHealthChanged(float newValue)
     {
-        if (_changeValue != null)
-            StopCoroutine(_changeValue);
-
-        _changeValue = StartCoroutine(ChangeValue(newValue));
-    }
-
-    private IEnumerator ChangeValue(float newValue)
-    {
-        while (_filled != newValue)
-        {
-            _slider.value = Mathf.MoveTowards(_filled, newValue, _speed * Time.deltaTime);
-            _filled = _slider.value;
-            yield return null;
-        }
+        _canvasGroup.DOFade(FadeInValue, _fadeDuration);
+        _slider.DOValue(newValue, _changeDuration);
+        _isTimerOn = true;
+        _timer = _timerDuration;
     }
 }
