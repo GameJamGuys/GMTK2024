ca826e9a35a9ea2cfedb1f7eacc8e004419d72b0
Mikhail Ostronosov <supermanmgdn@gmail.com>
2024-08-18 19:26:32 +0300

    Added MainMenu Background

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
+    private readonly float NewScale = 1f;
+    private readonly float Duration = 1f;
     //[SerializeField] private Button playButton;
 
     [SerializeField] private Button startButton;
     //[SerializeField] private Button quitButton;
-    
+
+    private void OnEnable()
+    {
+        startButton.transform.DOScale(NewScale, Duration);
+    }
     private void Awake()
     {
         //playButton.onClick.AddListener(() => { Loader.Load(Loader.Scene.Gameplay); });
