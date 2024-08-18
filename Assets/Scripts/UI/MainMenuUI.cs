4dda5938c20b9ef5fd8acbe315dd132f78b76940
Mikhail Ostronosov <supermanmgdn@gmail.com>
2024-08-18 15:15:16 +0300

    Created cutscene sequence

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -4,22 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenuUI : MonoBehaviour {
-
-
+public class MainMenuUI : MonoBehaviour
+{
     [SerializeField] private Button playButton;
-    [SerializeField] private Button quitButton;
-
-
-    private void Awake() {
-        playButton.onClick.AddListener(() => {
-            Loader.Load(Loader.Scene.Gameplay);
-        });
-        quitButton.onClick.AddListener(() => {
-            Application.Quit();
-        });
+    //[SerializeField] private Button quitButton;
+    
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() => { Loader.Load(Loader.Scene.Gameplay); });
+        //quitButton.onClick.AddListener(() => { Application.Quit(); });
 
         Time.timeScale = 1f;
     }
-
 }
