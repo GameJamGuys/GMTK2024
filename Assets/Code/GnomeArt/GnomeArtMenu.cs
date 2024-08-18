6a489c7490e92269cafa2772e494db7940bf59f8
Aristarkh <45267374+Kerioth@users.noreply.github.com>
2024-08-18 20:00:28 +0300

    Art changes

--- Assets/Code/GnomeArt/GnomeArtMenu.cs
+++ Assets/Code/GnomeArt/GnomeArtMenu.cs
@@ -1,16 +1,65 @@
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class GnomeArtMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum OutfitType { Body, Hat, Beard, Strigs};
+
+    [SerializeField]
+    ImageCollection bodyoutfit;
+    [SerializeField]
+    ImageCollection hatoutfit;
+    [SerializeField]
+    ImageCollection beardoutfit;
+    [SerializeField]
+    ImageCollection stringoutfit;
+
+    public void UpBody() => SetFit(bodyoutfit, 1, OutfitType.Body);
+    public void DownBody() => SetFit(bodyoutfit, -1, OutfitType.Body);
+
+    public void UpHat() => SetFit(hatoutfit, 1, OutfitType.Hat);
+    public void DownHat() => SetFit(hatoutfit, -1, OutfitType.Hat);
+
+    public void UpBeard() => SetFit(beardoutfit, 1, OutfitType.Beard);
+    public void DownBeard() => SetFit(beardoutfit, -1, OutfitType.Beard);
+
+    public void UpString() => SetFit(stringoutfit, 1, OutfitType.Strigs);
+    public void DownString() => SetFit(stringoutfit, -1, OutfitType.Strigs);
+
+    public void SetFit(ImageCollection work, int step, OutfitType type)
     {
-        
+        work.fit += step;
+        if (work.fit >= work.bodyBuild.Length)
+        {
+            work.fit = 0;
+        }
+
+        work.image.sprite = work.bodyBuild[work.fit];
+
+        switch (type)
+        {
+            case OutfitType.Body:
+                GnomeArtData.BODY = work.fit;
+                break;
+            case OutfitType.Hat:
+                GnomeArtData.HAT = work.fit;
+                break;
+            case OutfitType.Beard:
+                GnomeArtData.BEARD = work.fit;
+                break;
+            case OutfitType.Strigs:
+                GnomeArtData.STRINGS = work.fit;
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+}
+
+[System.Serializable]
+public class ImageCollection
+{
+    public Sprite[] bodyBuild;
+    public Image image;
+    public int fit = 0;
 }
--- Assets/Code/GnomeArt/GnomeArtSetter.cs
+++ Assets/Code/GnomeArt/GnomeArtSetter.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class GnomeArtSetter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    List<SpriteCollection> outfits;
+
+    //public void ChangeOutfit()
+
+    private void Start()
     {
-        
+        foreach(SpriteCollection collection in outfits)
+        {
+            switch (name)
+            {
+                case "body":
+                    collection.rend.sprite = collection.bodyBuild[GnomeArtData.BODY];
+                    break;
+                case "beard":
+                    collection.rend.sprite = collection.bodyBuild[GnomeArtData.BEARD];
+                    break;
+                case "hat":
+                    collection.rend.sprite = collection.bodyBuild[GnomeArtData.HAT];
+                    break;
+                case "strings":
+                    collection.rend.sprite = collection.bodyBuild[GnomeArtData.STRINGS];
+                    break;
+            }
+                
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+}
+
+[System.Serializable]
+public class SpriteCollection
+{
+    public string name;
+    public Sprite[] bodyBuild;
+    public SpriteRenderer rend;
+    public int fit;
 }
